af9a618b8088df383ea88ca91a38a12ccb13bb96
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-05 17:32:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(src, cb) {
+  let r = []
+
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return r;
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
